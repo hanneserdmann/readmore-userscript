7549381b98a95fbfdb05f0be63b58e00b6f77ac9
Hannes Erdmann <hannes.erdmann@gmail.com>
2014-06-19 22:14:34 +0200

    CHANGE: Frühe Version der Reload Posts und Content Klassen

---
+++ src/lib/Content.class.js
@@ -0,0 +1,58 @@
+/**
+ * Blablabla kommt Morgen, bin müde und will gleich pennen :D
+ */
+
+function Content(){
+
+    /**
+     * Hier werden die verschiedenen Möglichkeiten zum Abfragen definiert.
+     * Denke elements und selector sind selbsterklärend.
+     * @type {{forumPosts: {selector: string, elements: Array}}}
+     * @private
+     */
+    var _content = {
+        'forumPosts': {
+            'selector': 'div.forum_post',
+            'elements': []
+        }
+    };
+
+    /**
+     * Als Name wird hier einfach ein String übergeben, der auf die erste Ebene in dem _content Object passt. Wird KEIN
+     * context Parameter übergeben, werden die Daten aus dem aktuellen DOM geladen und zwischengespeichert.
+     * Mit Context (was z.B. bei übergabe von Daten die per Ajax geholt werden nützlich ist), wird nicht
+     * gecached und mit $(context).find() gearbeitet.
+     * @param name
+     * @param context
+     * @returns {Array}
+     */
+    this.get = function(name, context){
+        context = typeof context !== 'undefined' ? context : '';
+
+        var returnValue = [];
+
+        if (_content.hasOwnProperty(name)){
+            // Falls das Array leer ist oder ein Kontext übergeben wurde
+            if (!_content[name].elements.length || context !== ''){
+                // Daten auslesen
+                returnValue = _selectElement(name, context);
+
+                // Kein Kontext -> Cachen
+                if (context === ''){
+                    _content[name].elements = returnValue;
+                }
+            }
+            // Kein Kontext und das Element wurde bereits selektiert
+            else{
+                returnValue = _content[name].elements;
+            }
+        }
+
+        return returnValue;
+    };
+
+    var _selectElement = function(name, context){
+        if (context === '')     return $(_content[name].selector);
+        else                    return $(context).find(_content[name].selector);
+    };
+};
--- src/lib/ReadmoreUserscript.class.js
+++ src/lib/ReadmoreUserscript.class.js
@@ -1,6 +1,8 @@
 function ReadmoreUserscript() {
     var _options        = new Options();
     var _siteLocation   = new SiteLocation();
+    var _content        = new Content();
+    var _reloadPosts    = new ReloadPosts(_content);
 
     this.start = function () {
 
@@ -9,6 +11,19 @@ function ReadmoreUserscript() {
     };
 
     this.startIntervalReloadPosts = function () {
+        // Prüfen ob die Option gesetzt ist
+        if (_options.getOption('middleColumn_forum_reloadPosts_readNewPosts') === 'checked') {
+
+            // Prüfen ob wir uns im Forum befinden
+            if (_siteLocation.getLocation('forums') && _content.get('forumPosts').length){
+                // Nachladen von Posts vorbereiten
+                _reloadPosts.init();
+
+                setInterval(function(){
+                    _reloadPosts.readNewPosts();
+                }, (parseInt(_options.getOption('middleColumn_forum_reloadPosts_timeToWait'), 10) > 2) ? parseInt(_options.getOption('middleColumn_forum_reloadPosts_timeToWait'), 10) * 1000 : 3000);
+            }
+        }
     };
 
     this.startInvervalRapid = function () {
