a3615532a89c84e34592f30e41c25e2d3d465dd3
Hannes Erdmann <hannes.erdmann@gmail.com>
2014-06-23 22:10:09 +0200

    CHANGE: VERSION 4.0.2

--- Gruntfile.js
+++ Gruntfile.js
@@ -139,7 +139,7 @@ module.exports = function (grunt) {
                  options: {
                     replacements: [{
                         pattern: /\{\{version\}\}/ig,
-                        replacement: '4.0.1'
+                        replacement: '4.0.2'
                     }]
                 },
                 files: {
